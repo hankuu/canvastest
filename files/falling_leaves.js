584a6fa104bd34467d66af0e50e911331e0b69f9
hankuu <hanna.kumpula@gmail.com>
2018-10-07 10:21:51 +0300

    Falling leaves has the squares falling, staying and disappearing

--- files/falling_leaves.js
+++ files/falling_leaves.js
@@ -3,7 +3,7 @@ let winH = window.innerHeight;
 let winW = window.innerWidth;
 
 //Constant values
-const numSquares = 200;
+const numSquares = 1;
 const colorMatrix = [
   [190,30,34,.9],
   [190,83,18,.9],
@@ -12,10 +12,8 @@ const colorMatrix = [
 ];
 
 //Universal properties for every square
-let dx = 4;
-let dy = 4;
 let dang = 1;
-let colorPulse = 1;
+let colorPulse = -1;
 
 //Create Canvas
 const canvas = d3
@@ -67,28 +65,35 @@ function resize() {
 Rotating Square
   x     : middle point x-coordinate
   y     : middle point y-coordinate
+  dx    : velocity in x-direction
+  dy    : velocity in y-coordinate
   ang   : rotation angle of the square (degrees)
   side  : side length of the square
   color : color of the square
 */
-function RotatingSquare(x, y, ang, side, color) {
+function RotatingSquare(x, y, dx, dy, ang, side, color) {
+  //All squares begin as falling
+  this.falling = true;
+
+  //position
   this.x = x;
   this.y = y;
   this.ang = ang;
+
+  //sideLength
   this.side = side;
-  this.color = color;
+
+  //velocity attributes
   this.velocity = {
     x: dx,
     y: dy,
     rotation: dang
   };
+
+  //color changing value
   this.colorPulse = colorPulse;
-  this.originalColor = {
-    r: color.r,
-    g: color.g,
-    b: color.b,
-    a: color.a
-  }
+
+  //current color
   this.currentColor = {
     r: color.r,
     g: color.g,
@@ -117,63 +122,57 @@ RotatingSquare.prototype.draw = function() {
 //Update the rotating square's position
 RotatingSquare.prototype.update = function() {
 
-  //Reset angle around full circles
-  if (this.ang > 360 && this.velocity.dang > 0) {
-    this.ang = 0;
-  } else if (this.ang < 0 && this.velocity.dang < 0) {
-    this.ang = 360;
-  }
-
-  //Check window left&right
+  //Check window bottom. Leaves stay and turn to black
   //NOTE! Does not take into account if corner happens to cross the border
-  if (this.x > canvas.node().width - this.side / 2 || this.x < this.side / 2) {
-    this.velocity.x = -this.velocity.x;
-    this.velocity.rotation = -this.velocity.rotation;
-  }
-
-  //Check window top&bottom
-  //NOTE! Does not take into account if corner happens to cross the border
-  if (this.y > canvas.node().height - this.side / 2 || this.y < this.side / 2) {
-    this.velocity.y = -this.velocity.y;
-    this.velocity.rotation = -this.velocity.rotation;
-  }
-
-  //Add velocity
-  this.x += this.velocity.x;
-  this.y += this.velocity.y;
-  this.ang += this.velocity.rotation;
-
-
-  //Color pulsing
-  if(this.currentColor.r === this.originalColor.r &&
-  this.currentColor.g === this.originalColor.g &&
-this.currentColor.b === this.originalColor.b) {
-
-    this.colorPulse = -this.colorPulse;
-
-  }else if(this.currentColor.r === 0 &&
-  this.currentColor.g === 0 &&
-this.currentColor.b === 0){
-
-    this.colorPulse = -this.colorPulse;
-  }
-
-  // add colorPulse
-if(this.colorPulse < 0 && this.currentColor.r >0
-  || this.colorPulse > 0 && this.currentColor.r < this.originalColor.r){
-       this.currentColor.r += this.colorPulse;
+  if (this.y > canvas.node().height - this.side / 2) {
+    this.falling = false;
   }
 
 
-  if(this.colorPulse < 0 && this.currentColor.g >0
-    || this.colorPulse > 0 && this.currentColor.g < this.originalColor.g){
-         this.currentColor.g += this.colorPulse;
+  if(this.falling){
+    //Reset angle around full circles
+    if (this.ang > 360 && this.velocity.dang > 0) {
+      this.ang = 0;
+    } else if (this.ang < 0 && this.velocity.dang < 0) {
+      this.ang = 360;
     }
 
-    if(this.colorPulse < 0 && this.currentColor.b >0
-      || this.colorPulse > 0 && this.currentColor.b < this.originalColor.b){
-           this.currentColor.b += this.colorPulse;
-      }
+    //Check window left&right: bounce from sides
+    //NOTE! Does not take into account if corner happens to cross the border
+    if (this.x > canvas.node().width - this.side / 2 || this.x < this.side / 2) {
+      this.velocity.x = -this.velocity.x;
+      this.velocity.rotation = -this.velocity.rotation;
+    }
+
+    //Add velocity
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.ang += this.velocity.rotation;
+
+  }else{
+    //Color change to black
+      //this.colorPulse = -this.colorPulse;
+
+    // add colorPulse
+    if(this.colorPulse < 0 && this.currentColor.r >0){
+      this.currentColor.r += this.colorPulse;
+    }
+
+    if(this.colorPulse < 0 && this.currentColor.g >0){
+      this.currentColor.g += this.colorPulse;
+    }
+
+    if(this.colorPulse < 0 && this.currentColor.b >0){
+      this.currentColor.b += this.colorPulse;
+    }
+
+    //reduce size
+    if(this.side>0){
+      this.side -= 0.25;
+    }
+    //Keep square where it is respect to size
+    this.y = canvas.node().height - this.side / 2;
+  }
 
   //Everything set, draw the square  in the new position
   this.draw();
@@ -181,6 +180,8 @@ if(this.colorPulse < 0 && this.currentColor.r >0
 
 //Initialize
 let rSquares;
+let ticker = 0;
+let newSquareRate = 50;
 function init() {
   rSquares = [];
 
@@ -189,33 +190,88 @@ function init() {
     //Generate random values for the squares
     var sideLength = randomIntFromInterval(30, 100);
     var xPos = randomIntFromInterval(sideLength, canvas.node().width - sideLength);
-    var yPos = randomIntFromInterval(sideLength, canvas.node().height - sideLength);
+    var yPos = randomIntFromInterval(-20, -5);
+
+    var dirX = randomIntFromInterval(-3,3);
+    while(dirX==0){
+      dirX = randomIntFromInterval(-3,3);
+    }
+
+    var dirY = randomIntFromInterval(1,3);
+
     var startAngle = randomIntFromInterval(0, 359);
 
     var colorIndex = randomIntFromInterval(0, colorMatrix.length - 1);
 
     rSquares.push(
-      new RotatingSquare(xPos, yPos, startAngle, sideLength, constructColorFromIndex(colorIndex))
+      new RotatingSquare(xPos, yPos, dirX, dirY, startAngle, sideLength, constructColorFromIndex(colorIndex))
     );
   }
 }
 
-
 //This function makes things move
-function animate() {
-  window.requestAnimationFrame(animate);
+//TODO fix flickering
+var fps = 60;
+function animate(timestamp) {
+
+  setTimeout(function(){
+    window.requestAnimationFrame(animate);
+
   //Clear the whole window, not just canvas
   context.clearRect(0, 0, winW, winH);
 
   //Update rotating rSquares
-  rSquares.forEach(rSquare => {
+  rSquares.forEach((rSquare, index) => {
     rSquare.update();
+    //Remove squares with no side length left
+    if(rSquare.side==0){
+      rSquares.splice(index,1);
+    }
   });
 
-}
+  //increase ticker
+  ticker++;
+
+  //when ticker reaches a value divisible by newSquareRate, create new squares
+  if(ticker % newSquareRate == 0){
+    //Generate random number of squares
+    var howManySquares = randomIntFromInterval(1,5);
+
+    for(var i=0; i<howManySquares; i++){
+      var sideLength = randomIntFromInterval(30, 100);
+      var xPos = randomIntFromInterval(sideLength, canvas.node().width - sideLength);
+      var yPos = randomIntFromInterval(-20, -5);
+
+      var dirX = randomIntFromInterval(-3,3);
+      while(dirX==0){
+        dirX = randomIntFromInterval(-3,3);
+      }
+
+      var dirY = randomIntFromInterval(1, 3);
+
+      var startAngle = randomIntFromInterval(0, 359);
+
+      var colorIndex = randomIntFromInterval(0, colorMatrix.length - 1);
+
+      rSquares.push(
+        new RotatingSquare(xPos, yPos, dirX, dirY, startAngle, sideLength, constructColorFromIndex(colorIndex))
+      );
+    }
+
+    //reset the interval for creating new leaves
+    newSquareRate = randomIntFromInterval(25,100);
+    //reset ticker
+    ticker = 0;
+  }
+
+}, 1000/fps) //setTimeout
+
+//  window.requestAnimationFrame(animate);
+} //animate()
 
 
 //Initialize
 init();
 //make things move
-animate();
+//animate();
+window.requestAnimationFrame(animate);
